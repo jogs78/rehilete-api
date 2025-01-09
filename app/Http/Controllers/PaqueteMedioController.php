6feb32c7a4d7952eadd7f86b2cbe830c711377ea
M.C. Jorge Octavio Guzmán Sánchez <jogs78@gmail.com>
2025-01-09 15:06:32 -0600

    ServicioMedioController agrega de uno en uno

--- app/Http/Controllers/PaqueteMedioController.php
+++ app/Http/Controllers/PaqueteMedioController.php
@@ -28,35 +28,20 @@ class PaqueteMedioController extends Controller
         $this->authorize('create', Medio::class);
         Log::channel('debug')->info('store de medio en paquetes.');
         Log::channel('debug')->info('al paquete:'.$paquete->toJson());
-        //Log::channel('debug')->info(json_encode($request->file('imagenes')));
-
         $usuario = Auth::getUser();
-        $agregados = [];
         $medio = $request->file('imagen');
             Log::channel('debug')->info('IMAGEN x.');
             $nombre = time().rand(1, 100).'.'.$medio->extension();
             $medio->storeAs('', $nombre, 'publicas');
-            //Storage::disk('publicas')->putFile('', $medio, $nombre);
             $imagen = new Medio;
             $imagen->ruta = $nombre;
             $imagen->nombre = $medio->getClientOriginalName();
             $imagen->usuario_id = $usuario->id;
             $imagen->descripcion = $request->descripcion;
             $imagen->save();
-//            array_push($agregados, $imagen->id);
-        /*
-        ob_start();
-        var_dump($agregados);
-        $cadena_var_dump = ob_get_clean();
-        Log::channel('debug')->info('agregados: ' . $cadena_var_dump );
-        */
         $paquete->imagenes()->attach($imagen->id);
-//        $paquete->load('imagenes');
-
         $ultimaImagen = $paquete->imagenes()->orderBy('created_at', 'desc')->first();
         return response()->json($ultimaImagen);
-
-//        return response()->json($paquete->imagenes()->select('medios.id', 'medios.nombre')->get());
     }
 
     /**
@@ -88,3 +73,10 @@ class PaqueteMedioController extends Controller
         }
     }
 }
+
+        /*
+        ob_start();
+        var_dump($agregados);
+        $cadena_var_dump = ob_get_clean();
+        Log::channel('debug')->info('agregados: ' . $cadena_var_dump );
+        */
--- app/Http/Controllers/ServicioMedioController.php
+++ app/Http/Controllers/ServicioMedioController.php
@@ -27,34 +27,20 @@ class ServicioMedioController extends Controller
         $this->authorize('create', Medio::class);
         Log::channel('debug')->info('store de medio en servicios.');
         Log::channel('debug')->info('al servicio:'.$servicio->toJson());
-        //Log::channel('debug')->info(json_encode($request->file('imagenes')));
-
         $usuario = Auth::getUser();
-        $agregados = [];
-        foreach ($request->file('imagenes') as $medio) {
+        $medio = $request->file('imagen');
             Log::channel('debug')->info('IMAGEN x.');
             $nombre = time().rand(1, 100).'.'.$medio->extension();
             $medio->storeAs('', $nombre, 'publicas');
-            //Storage::disk('publicas')->putFile('', $medio, $nombre);
             $imagen = new Medio;
             $imagen->ruta = $nombre;
             $imagen->nombre = $medio->getClientOriginalName();
             $imagen->usuario_id = $usuario->id;
             $imagen->descripcion = $request->descripcion;
             $imagen->save();
-            array_push($agregados, $imagen->id);
-        }
-        Log::channel('debug')->info('agregados: '.implode(',', $agregados));
-        /*
-        ob_start();
-        var_dump($agregados);
-        $cadena_var_dump = ob_get_clean();
-        Log::channel('debug')->info('agregados: ' . $cadena_var_dump );
-        */
-        $servicio->imagenes()->attach($agregados);
-        $servicio->load('imagenes');
-
-        return response()->json($servicio->imagenes()->select('medios.id', 'medios.nombre')->get());
+        $servicio->imagenes()->attach($imagen->id);
+        $ultimaImagen = $servicio->imagenes()->orderBy('created_at', 'desc')->first();
+        return response()->json($ultimaImagen);
     }
 
     /**
@@ -85,3 +71,10 @@ class ServicioMedioController extends Controller
         }
     }
 }
+
+        /*
+        ob_start();
+        var_dump($agregados);
+        $cadena_var_dump = ob_get_clean();
+        Log::channel('debug')->info('agregados: ' . $cadena_var_dump );
+        */
